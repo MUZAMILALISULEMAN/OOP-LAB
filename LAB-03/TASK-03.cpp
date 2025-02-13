c623b3ae7c1b105e75f4ab24ff855e860f0965f1
Muzamil Suleman <muzamilworkspace@gmail.com>
2025-02-13 11:52:13 -0800

    TASK-03

---
+++ LAB-03/TASK-03.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+using namespace std;
+class Library {
+	private:
+		string bookList[100];
+		string borrowedBooks[100];
+		string students[100];
+		int availableBooks[100];
+		int books = 0;
+		int borrowedBooksCount = 0;
+	public:
+		void addBook(string bookName) {
+			bookList[books] = bookName;
+			availableBooks[books] = 1;
+			books++;
+			cout<<"BOOK ADDED!"<<endl;
+		}
+		void lendBook(string bookName,string studentName) {
+			for(int i=0; i<books; i++) {
+				if(bookList[i] == bookName && availableBooks[i] == 1) {
+					cout<<"BOOK ISSUED TO "<<studentName<<endl;
+					availableBooks[i] = 0;
+					students[borrowedBooksCount] = studentName;
+					borrowedBooks[borrowedBooksCount] = bookName;
+					borrowedBooksCount++;
+					break;
+				}
+			}
+		}
+		void returnBook(string bookName,string studentName) {
+			int found = 0;
+			for(int i=0; i<borrowedBooksCount; i++) {
+				if(borrowedBooks[i] == bookName) {
+				
+					found = 1;
+					
+					for(int j=i;j<borrowedBooksCount-1;j++){
+
+						borrowedBooks[j] = borrowedBooks[j+1];
+						students[j] = students[j+1];
+						
+					}
+					borrowedBooksCount--;
+					break;
+					
+				}
+			}
+			if(found == 0) {
+				return;
+			}
+			for(int i=0; i<books; i++) {
+				if(bookList[i] == bookName) {
+					availableBooks[i] = 1;
+					break;
+				}
+			}
+
+			cout<<"BOOK RETURNED BY "<<studentName<<endl;
+
+		}
+		void AvailableBooks(){
+			int k = 0;
+			cout<<"AVAILABLE BOOKS:";
+			for(int i=0;i<books;i++){
+	
+				if(availableBooks[i] == 1){
+					cout<<endl<<++k<<". "<<bookList[i];
+					
+				}
+				
+				
+			}
+			if(k == 0){
+				cout<<" NO BOOK AVAILABLE";
+			}
+			cout<<endl;
+		}		
+
+};
+int main() {
+	
+	Library FAST;
+	FAST.addBook("STRANGER THINGS");
+	FAST.AvailableBooks();
+	FAST.addBook("HARRY POTTER");
+	FAST.AvailableBooks();
+	FAST.lendBook("HARRY POTTER","ALI");
+	FAST.AvailableBooks();
+	FAST.returnBook("HARRY POTTER","ALI");
+	FAST.AvailableBooks();
+
+	return 0;
+}
